e61c6fec4b93a36b333a94425f91fe0730570d8e
cgythm <michael@cgythm.com>
2015-08-30 19:23:13 -0400

    error and mainteance pages

---
+++ wakeup_norma.js
@@ -0,0 +1,16 @@
+(function(){
+
+    function norma_asleep(){
+        //this is when norma is asleep and doesn't wanna wake up.
+        //norma needs to sleep, so it won't wake up until it has gotten 6 hours of sleep.
+        //we are within this process until it has waken up
+
+        return 15*60*60*1000; //next time to retry.
+    }
+
+
+    function step(){
+
+    };
+
+})();
